266f91c56eb19ba48a59f143acf68149c869986a
son.geon <son.geon@jtbc.co.kr>
2022-11-17 11:29:33 +0900

    모듈 세팅 변경

---
+++ quant-api/src/main/java/com/stock/quant/api/consts/ApplicationConstants.java
@@ -0,0 +1,18 @@
+package com.stock.quant.api.consts;
+
+public class ApplicationConstants {
+
+    private ApplicationConstants() {
+        throw new IllegalAccessError("어플리케이션에서 공통으로 사용되는 상수 클래스로 생성자를 사용할 수 없습니다.");
+    }
+
+    public static final int PAGE_SIZE = 1000;
+
+    public static final String API_GO_URL = "apis.data.go.kr";
+    public static final String KRX_STOCK_LIST_URI = "1160100/service/GetKrxListedInfoService/getItemInfo";
+
+    public static final String KRX_STOCK_FINANCE_URI = "1160100/service/GetFinaStatInfoService";
+
+    public static final String KRX_STOCK_VALUE_URI = "1160100/service/GetStockSecuritiesInfoService";
+
+}
