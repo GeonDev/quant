09a8b92efb11b540d0f23fb0e8046d6cda9f26d7
son.geon <son.geon@jtbc.co.kr>
2023-05-26 14:56:14 +0900

    쿼리 수정

--- stock-service/src/main/resources/shema.sql
+++ stock-service/src/main/resources/shema.sql
@@ -1,43 +1,50 @@
-CREATE TABLE tb_corp_code (
+CREATE TABLE IF NOT EXISTS tb_corp_code (
   corp_code varchar(11) NOT NULL,
   corp_name text,
-  stock_code varchar(11) DEFAULT NULL,
+  stock_code varchar(11) ,
   corp_state varchar(5) DEFAULT 'Y' COMMENT 'Y : 정상 , N : 거래정지 또는 상장 폐지',
   PRIMARY KEY (corp_code)
-)
+);
 
 
-CREATE TABLE tb_corp_finance (
+CREATE TABLE IF NOT EXISTS tb_corp_finance (
   rcept_no varchar(11) NOT NULL,
-  corp_code varchar(11) DEFAULT NULL,
-  stock_code varchar(11) DEFAULT NULL,
-  start_dt date DEFAULT NULL,
-  end_dt date DEFAULT NULL,
-  capital bigint(20) DEFAULT NULL,
-  total_assets bigint(20) DEFAULT NULL,
-  total_debt bigint(20) DEFAULT NULL,
-  total_equity bigint(20) DEFAULT NULL,
-  revenue bigint(20) DEFAULT NULL,
-  operating_profit bigint(20) DEFAULT NULL,
-  net_income bigint(20) DEFAULT NULL,
-  earned_surplus bigint(20) DEFAULT NULL,
+  corp_code varchar(11) ,
+  stock_code varchar(11) ,
+  start_dt date ,
+  end_dt date ,
+  capital bigint,
+  total_assets bigint,
+  total_debt bigint,
+  total_equity bigint,
+  revenue bigint,
+  operating_profit bigint,
+  net_income bigint,
+  earned_surplus bigint,
   PRIMARY KEY (rcept_no)
-)
+);
 
 
-CREATE TABLE tb_stock_price (
+CREATE TABLE IF NOT EXISTS tb_stock_price (
   stock_code varchar(11) NOT NULL,
-  market_code varchar(11) DEFAULT NULL,
-  bas_dt date DEFAULT NULL,
-  volume int(11) DEFAULT NULL,
-  start_price int(11) DEFAULT NULL,
-  end_price int(11) DEFAULT NULL,
-  high_price int(11) DEFAULT NULL,
-  low_price int(11) DEFAULT NULL,
-  daily_range decimal(11,0) DEFAULT NULL,
-  daily_ratio decimal(11,2) DEFAULT NULL,
-  stock_total_cnt bigint(11) DEFAULT NULL,
-  market_total_amt bigint(11) DEFAULT NULL,
+  market_code varchar(11) ,
+  bas_dt date ,
+  volume int ,
+  start_price int ,
+  end_price int ,
+  high_price int ,
+  low_price int ,
+  daily_range decimal(11,0) ,
+  daily_ratio decimal(11,2) ,
+  stock_total_cnt bigint ,
+  market_total_amt bigint ,
   PRIMARY KEY (stock_code)
-)
+);
 
+CREATE TABLE IF NOT EXISTS tb_stock_price_average (
+      stock_code varchar(11) NOT NULL,
+      tar_dt date ,
+      price_type varchar(11) ,
+      price int ,
+      PRIMARY KEY (stock_code)
+);
