839ab187fcbb8c49f2294437c2ee9bd6ef10e509
son.geon <son.geon@jtbc.co.kr>
2023-06-16 18:01:45 +0900

    배치 설정 중

--- quant-batch/src/main/java/com/quant/batch/job/ExampleJobConfig.java
+++ quant-batch/src/main/java/com/quant/batch/job/ExampleJobConfig.java
@@ -1,94 +1,63 @@
 package com.quant.batch.job;
 
-import com.finnq.batch.model.Member;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.*;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
-import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManagerFactory;
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- * 전체 금액이 10,000원 이상인 회원들에게 1,000원 캐시백을 주는 배치
- */
 
 @Slf4j
 @Configuration
 @EnableBatchProcessing
+@RequiredArgsConstructor
 public class ExampleJobConfig {
 
-    @Autowired public JobBuilderFactory jobBuilderFactory;
-    @Autowired public StepBuilderFactory stepBuilderFactory;
-    @Autowired public EntityManagerFactory entityManagerFactory;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job ExampleJob() throws Exception {
-
-        Job exampleJob = jobBuilderFactory.get("exampleJob")
-                .start(Step())
-                .build();
-
-        return exampleJob;
-    }
-
-    @Bean
-    @JobScope
-    public Step Step() throws Exception {
-        return stepBuilderFactory.get("Step")
-                .<Member,Member>chunk(10)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
+    public Job stepNextJob() {
+        return jobBuilderFactory.get("stepNextJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
                 .build();
     }
 
     @Bean
-    @StepScope
-    public JpaPagingItemReader<Member> reader() throws Exception {
-
-        Map<String,Object> parameterValues = new HashMap<>();
-        parameterValues.put("amount", 10000);
-
-        return new JpaPagingItemReaderBuilder<Member>()
-                .pageSize(10)
-                .parameterValues(parameterValues)
-                .queryString("SELECT p FROM Member p WHERE p.amount >= :amount ORDER BY id ASC")
-                .entityManagerFactory(entityManagerFactory)
-                .name("JpaPagingItemReader")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
     @Bean
-    @StepScope
-    public ItemProcessor<Member, Member> processor(){
-
-        return new ItemProcessor<Member, Member>() {
-            @Override
-            public Member process(Member member) throws Exception {
-
-                //1000원 추가 적립
-                member.setAmount(member.getAmount() + 1000);
-
-                return member;
-            }
-        };
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
     }
 
     @Bean
-    @StepScope
-    public JpaItemWriter<Member> writer(){
-        return new JpaItemWriterBuilder<Member>()
-                .entityManagerFactory(entityManagerFactory)
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step3");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 }
