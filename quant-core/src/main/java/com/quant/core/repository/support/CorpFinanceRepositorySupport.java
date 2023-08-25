60965335aa96853691100d36d41435e415d9a751
SonGeon <geons.dev@gmail.com>
2023-08-26 00:24:43 +0900

    동적 오더 기본 세팅

--- quant-core/src/main/java/com/quant/core/repository/support/CorpFinanceRepositorySupport.java
+++ quant-core/src/main/java/com/quant/core/repository/support/CorpFinanceRepositorySupport.java
@@ -1,10 +1,15 @@
 package com.quant.core.repository.support;
 
 import com.quant.core.entity.CorpFinance;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.quant.core.entity.QCorpFinance.corpFinance;
 
@@ -19,4 +24,13 @@ public class CorpFinanceRepositorySupport extends QuerydslRepositorySupport {
         this.queryFactory = queryFactory;
     }
 
+
+    private OrderSpecifier[] createOrderSpecifier() {
+
+        List<OrderSpecifier> orderSpecifiers = new ArrayList<>();
+        orderSpecifiers.add(new OrderSpecifier(Order.DESC, corpFinance.YOY));
+
+
+        return orderSpecifiers.toArray(new OrderSpecifier[orderSpecifiers.size()]);
+    }
 }
