0a50916d723e1ae67aba08499afb420213c1bfb7
son.geon <son.geon@jtbc.co.kr>
2023-11-17 09:48:18 +0900

    restTemplate 연결 재시도 코드 추가

--- quant-core/src/main/java/com/quant/core/config/RestTemplateConfig.java
+++ quant-core/src/main/java/com/quant/core/config/RestTemplateConfig.java
@@ -1,21 +1,35 @@
 package com.quant.core.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(5000);
-        requestFactory.setReadTimeout(8000);
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(8))
+                .additionalInterceptors(clientHttpRequestInterceptor())
+                .build();
+    }
 
-        restTemplate.setRequestFactory(requestFactory);
-
-        return restTemplate;
+    //재시도 설정
+    public ClientHttpRequestInterceptor clientHttpRequestInterceptor() {
+        return (request, body, execution) -> {
+            RetryTemplate retryTemplate = new RetryTemplate();
+            retryTemplate.setRetryPolicy(new SimpleRetryPolicy(3));
+            try {
+                return retryTemplate.execute(context -> execution.execute(request, body));
+            } catch (Throwable throwable) {
+                throw new RuntimeException(throwable);
+            }
+        };
     }
 }
--- quant-core/src/main/java/com/quant/core/consts/ApplicationConstants.java
+++ quant-core/src/main/java/com/quant/core/consts/ApplicationConstants.java
@@ -8,11 +8,11 @@ public class ApplicationConstants {
 
     public static final String SPLIT_KEY = ",";
 
-    //오픈 dart의 최소 제공 연수
-    public static final int LIMIT_YEAR = 2015;
+    //OPEN DART 제공 연도(최소 2015년)
+    public static final int LIMIT_YEAR = 2020;
 
     //공공 데이터 포탈의 최대 열 개수(최대 1000 ~ 1500)
-    public static final int PAGE_SIZE = 750;
+    public static final int PAGE_SIZE = 1000;
 
     public static final String API_GO_URL = "apis.data.go.kr";
 
